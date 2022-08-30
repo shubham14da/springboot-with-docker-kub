5409a2614972a909e795ba30fd4e9fd077ead25a
shubham14da <103240682+shubham14da@users.noreply.github.com>
2022-08-30 17:10:43 +0530

    Update JhooqDockerDemoController.java

--- src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
@@ -8,6 +8,6 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Docker Demo - Hello Jhooq";
+        return "Docker Demo - Hello Jhooq done by SHUBHAM DANGI";
     }
 }
